dd3c6aba8b35aade9afdaa2dfcc4157dc49c801a
artwlcn <982563+artwl@users.noreply.github.com>
2020-05-20 13:37:05 +0800

    compress and resize

---
+++ compress.py
@@ -0,0 +1,15 @@
+import tinify
+import os
+
+tinify.key = 'n5LFT9F3g2xp54zRRPCZdsdyRPqnQD2Q'
+
+srcDir = "./src"
+
+desDir = "./compressed"
+
+imgs = os.listdir(srcDir)
+for img in imgs:
+    if ".jpeg" in img or ".jpg" in img or ".png" in img:
+        print(img)
+        tinify.from_file("%s/%s" % (srcDir, img)).to_file("%s/%s" % (desDir, img))
+print("done")
